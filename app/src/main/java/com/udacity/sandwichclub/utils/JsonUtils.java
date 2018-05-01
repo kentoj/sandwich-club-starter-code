0f97d32014e4eeed94ec194a95ccb4822aebf382
Kent O. Johnson <kentoj@gmail.com>
2018-04-30 19:15:07 -0600

    Address suggestion on keys

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,23 +11,32 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static String
+            SANDWICH_NAME = "name",
+            SANDWICH_MAIN_NAME = "mainName",
+            SANDWICH_ALSO_KNOWN_AS = "alsoKnownAs",
+            SANDWICH_PLACE_OF_ORIGIN = "placeOfOrigin",
+            SANDWICH_DESCRIPTION = "description",
+            SANDWICH_IMAGE = "image",
+            SANDWICH_INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
         try {
             JSONObject sandwichJSON = new JSONObject(json);
 
-            JSONObject name = sandwichJSON.getJSONObject("name");
-            String mainName = name.getString("mainName");
-            JSONArray aka = name.getJSONArray("alsoKnownAs");
+            JSONObject name = sandwichJSON.getJSONObject(SANDWICH_NAME);
+            String mainName = name.optString(SANDWICH_MAIN_NAME);
+            JSONArray aka = name.getJSONArray(SANDWICH_ALSO_KNOWN_AS);
             List<String> akaStr = new ArrayList<>();
             for (int i = 0; i < aka.length(); i++) {
                 akaStr.add(aka.get(i).toString());
             }
 
-            String placeOfOrigin = sandwichJSON.getString("placeOfOrigin");
-            String description = sandwichJSON.getString("description");
-            String image = sandwichJSON.getString("image");
+            String placeOfOrigin = sandwichJSON.getString(SANDWICH_PLACE_OF_ORIGIN);
+            String description = sandwichJSON.getString(SANDWICH_DESCRIPTION);
+            String image = sandwichJSON.getString(SANDWICH_IMAGE);
 
-            JSONArray ingredients = sandwichJSON.getJSONArray("ingredients");
+            JSONArray ingredients = sandwichJSON.getJSONArray(SANDWICH_INGREDIENTS);
             List<String> ingredientsStr = new ArrayList<>();
             for (int i = 0; i < ingredients.length(); i++) {
                 ingredientsStr.add(ingredients.get(i).toString());
